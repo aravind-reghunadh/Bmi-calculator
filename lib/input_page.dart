0ffa08dd358f4ddec53210753366fb92550ea549
aravind-reghunadh <aravind_reghunadh@ieee.org>
2021-05-24 20:31:36 +0530

    added the buttons

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -12,9 +12,51 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
+      body: Column(children: <Widget>[
+        Expanded(
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: ReusableCard(),
+              ),
+              Expanded(
+                child: ReusableCard(),
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          child: ReusableCard(),
+        ),
+        Expanded(
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: ReusableCard(),
+              ),
+              Expanded(
+                child: ReusableCard(),
+              ),
+            ],
+          ),
+        ),
+      ]),
+    );
+  }
+}
+
+class ReusableCard extends StatefulWidget {
+  @override
+  _ReusableCardState createState() => _ReusableCardState();
+}
+
+class _ReusableCardState extends State<ReusableCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+          color: Color(0xff1d1e33), borderRadius: BorderRadius.circular(10.0)),
     );
   }
 }
